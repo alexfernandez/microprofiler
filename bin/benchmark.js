8ac8b313aaa8c0f8dd5fcd679fdaf8062d71a072
Alex Fernández <alexfernandeznpm@gmail.com>
2023-08-25 13:03:07 +0200

    Add hrtime.bigint() to benchmark

--- bin/benchmark.js
+++ bin/benchmark.js
@@ -1,45 +1,68 @@
 import microprofiler from '../index.js'
 
+const iterations = 1e7
+
 
 function bare() {
 	const start = Date.now();
-	for (let i = 0; i < 1e7; i++) {
+	for (let i = 0; i < iterations; i++) {
 		// do nothing
 	}
 	const elapsed = Date.now() - start;
-	console.log('bare: %s', elapsed);
+	console.log(`Bare loop: ${elapsed} ms`);
 }
 
 function hrtime() {
 	const start = Date.now();
-	for (let i = 0; i < 1e7; i++) {
+	for (let i = 0; i < iterations; i++) {
 		process.hrtime();
 	}
 	const elapsed = Date.now() - start;
-	console.log('hrtime: %s', elapsed);
+	console.log(`process.hrtime(): ${elapsed} ms`);
+}
+
+function bigint() {
+	const start = Date.now();
+	for (let i = 0; i < iterations; i++) {
+		process.hrtime.bigint();
+	}
+	const elapsed = Date.now() - start;
+	console.log(`process.hrtime.bigint(): ${elapsed} ms`);
 }
 
 function dateNow() {
 	const start = Date.now();
-	for (let i = 0; i < 1e7; i++) {
+	for (let i = 0; i < iterations; i++) {
 		Date.now();
 	}
 	const elapsed = Date.now() - start;
-	console.log('dateNow: %s', elapsed);
+	console.log(`Date.now(): ${elapsed} ms`);
 }
 
-function profiled() {
+function profileStart() {
+	const startMs = Date.now();
+	for (let i = 0; i < iterations; i++) {
+		microprofiler.start();
+	}
+	const elapsed = Date.now() - startMs;
+	console.log(`start(): ${elapsed} ms`);
+}
+
+function profileMeasureFrom() {
 	const startMs = Date.now();
 	const start = microprofiler.start();
-	for (let i = 0; i < 1e7; i++) {
+	for (let i = 0; i < iterations; i++) {
 		microprofiler.measureFrom(start, 'test', 1e9);
 	}
 	const elapsed = Date.now() - startMs;
-	console.log('measureFrom: %s', elapsed);
+	console.log(`measureFrom(): ${elapsed} ms`);
 }
 
+console.log(`Milliseconds for ${iterations} iterations:`)
 bare();
 hrtime();
+bigint()
 dateNow();
-profiled();
+profileStart();
+profileMeasureFrom()
 
